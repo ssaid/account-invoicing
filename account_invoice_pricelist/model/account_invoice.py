86d73a2886a2ee37f65e0446480a23b63fd022dc
OdooProd-003-003 <odoo@e-mips.com.ar>
2020-09-30 13:11:09 -0300

    Code as in PRD-SRV

--- account_invoice_pricelist/model/account_invoice.py
+++ account_invoice_pricelist/model/account_invoice.py
@@ -60,3 +60,33 @@ class AccountInvoice(models.Model):
                 continue  # Should only be valid for draft invoices
             this.invoice_line.filtered('product_id').update_from_pricelist()
             this.button_reset_taxes()
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def _get_invoice_vals(self, key, inv_type, journal_id, move):
+        """Set pricelist from sale / purchase order if set on picking."""
+        inv_vals = super(StockPicking, self)._get_invoice_vals(
+            key, inv_type, journal_id, move
+        )
+        pricelist_id = False
+        picking = move.picking_id
+        if inv_type in ['out_invoice', 'out_refund']:
+            # Get the pricelist of the sale order, if defined
+            if self._model._columns.get('sale_id', False):
+                pricelist_id = picking.sale_id.pricelist_id.id
+
+        elif inv_type in ['in_invoice', 'in_refund']:
+            # Get the pricelist of the purchase order, if defined
+            if move._model._columns.get('purchase_line_id', False):
+                pricelist_id = move.purchase_line_id.order_id.pricelist_id.id
+
+        if not pricelist_id and picking.partner_id:
+            # Set the default pricelist of the partner
+            pricelist_id = picking.partner_id._get_invoice_pricelist_id(
+                inv_type)
+
+        inv_vals['pricelist_id'] = pricelist_id
+
+        return inv_vals
